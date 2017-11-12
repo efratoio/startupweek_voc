031af5e2824182e657a2a06d81e11f0648af4fc6
Soana (Andra Rübsteck) <andra@ruebsteck.de>
2017-11-12 11:12:20 +0100

    process speech

--- client/src/api/transactions.ts
+++ client/src/api/transactions.ts
@@ -6,3 +6,12 @@ export type TransactionsResult = string[][];
 export async function transactions(token: string): Promise<TransactionsResult> {
     return parseApi<TransactionsResult>(fetch(url(`/get?user=${token}`)));
 }
+
+export async function processSpeech(data: string): Promise<Transaction> {
+    const body = new FormData();
+    body.append("data", data);
+    return parseApi<Transaction>(fetch(url(`/process`), {
+        method: "POST",
+        body
+    }));
+}
--- client/src/pages/add/add.tsx
+++ client/src/pages/add/add.tsx
@@ -1,13 +1,28 @@
 import * as React from "react";
 
+import { inject, external } from "tsdi";
+import { observer } from "mobx-react";
+import { action, observable, computed } from "mobx";
+
+import { TransactionsStore } from "store";
+
 import { SpeechRecognitionOverlay } from "ui";
 
+@observer @external
 export class PageAdd extends React.Component {
+	@inject private transactions: TransactionsStore;
+
 	public render(): JSX.Element {
+		const { unsavedTransaction } = this.transactions;
 		return (
 			<div>
 				<h1> add entry</h1>
-				<SpeechRecognitionOverlay />
+				{
+					!unsavedTransaction ? <SpeechRecognitionOverlay /> : null
+				}
+				{
+					unsavedTransaction ? JSON.stringify(unsavedTransaction) : null
+				}
 			</div>
 		)
 	}
